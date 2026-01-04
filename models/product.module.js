f7ff7917b8835cfb34016def5d98df7939f4a646
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-01-04 16:01:34 +1100

    configure database

---
+++ models/product.module.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// this is how a product should look like 
+const productSchema = new mongoose.Schema({
+    title: String,
+    description: String, 
+    price: Number,
+    discountPercentage: Number,
+    stock: Number, 
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean
+});
+
+//            model name (used in code) - rules to follow - mongoDB collection name
+const Product = mongoose.model("Product", productSchema, "products");
+// Product is the tool to talk to the "products" collection 
+// now they can:
+//     + add product
+//     + get product 
+//     + update product 
+//     + delete product 
+
+module.exports = Product;
