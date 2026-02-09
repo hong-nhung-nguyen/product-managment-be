c14910c2c17d26d8f70d8eef32308d8210f287c7
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-02-09 22:50:28 +0700

    Optimize the FilterStatus group

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,42 +1,24 @@
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helpers/filterStatus");
+
 // [GET] /admin/products
 
 module.exports.index = async (req, res) => {
 
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: ""
-        }
-    ]
+    // Đoạn bộ lọc
 
-    if (req.query.status) {
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    } else {
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-    }
+    const filterStatus = filterStatusHelper(req.query);
+
+    
+    let find = {
+        deleted: false
+    };
 
     if(req.query.status) {
         find.status = req.query.status
     }
 
-    let find = {
-        deleted: false
-    };
 
     let keyword = "";
 
@@ -51,6 +33,10 @@ module.exports.index = async (req, res) => {
     const products = await Product.find(find);
 
     // console.log(products);
+
+
+
+
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
