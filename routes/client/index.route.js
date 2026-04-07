ac43df4621af4c6837c69073c9a91a4e668bb0e6
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-04-07 22:11:58 +1000

    Search function for client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,5 +1,6 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
@@ -8,6 +9,8 @@ module.exports = (app) => {
     app.use(categoryMiddleware.category);
 
     app.use("/", homeRoutes);
+    
+    app.use("/search", searchRoutes);
 
     app.use("/products", productRoutes);
 }
