a2dfce5682f9ca5a540a88d6c568b233014cb4a9
Hong Nhung <137052444+rose-de-pur-nguyen@users.noreply.github.com>
2026-04-07 23:39:29 +1000

    Create Cart model

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,9 +3,11 @@ const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 module.exports = (app) => {
     // this middleware can be used for every routes
+    app.use(cartMiddleware.cartId);
     app.use(categoryMiddleware.category);
 
     app.use("/", homeRoutes);
